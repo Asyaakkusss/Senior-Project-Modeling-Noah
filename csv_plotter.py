95a75545de29ca596d523356716ed939ed3f39d9
NoahHenriques <noah.henriq@gmail.com>
2024-09-24 02:20:34 -0400

    csv_plotter helps visualize data
    Signed-off-by: NoahHenriques <noah.henriq@gmail.com>

--- csv_plotter.py
+++ csv_plotter.py
@@ -1,13 +1,30 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def plot(file, interval):
+#interval is set to 1440 as that is the number of minutes in a day
+#stop is in case you want to stop plotting after a specific time
+#begin is if you want to start later in the file
+def plot(file, interval=1440, stop=None, begin=0):
     df = pd.read_csv(file)
-    ax = plt.gca()
-    row = interval
-    while row >= df.shape[0]:
-        plot_df = df.iloc[:row]
-        plot_df.plot(kind='line', x='time', y='value')
-        plt.show()
 
-plot("PureHeartRate.csv", 200)
+    if 'time' not in df.columns or 'value' not in df.columns:
+        raise ValueError("The CSV must contain 'time' and 'value' columns.")
+    
+    prev_row = begin
+    row = prev_row + interval
+    if not stop:
+        stop = df.shape[0]
+    while row <= stop:
+        plot_df = df.iloc[prev_row:row]
+        
+        # Plot the data
+        plot_df.plot(kind='line', x='time', y='value', ax=plt.gca())
+        plt.title(f"Data from {prev_row} to {row}")
+        plt.show()
+        
+        # Increase the row limit for the next plot
+        prev_row = row
+        row += interval
+
+#Change the data as necessary
+plot('PureHeartRate.csv', stop=10000)
