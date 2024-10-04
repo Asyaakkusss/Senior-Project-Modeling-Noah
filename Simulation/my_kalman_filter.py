d8c0e7c91230165b83331c24c57a745e2c56df56
Asya Akkus <aya29@case.edu>
2024-10-04 04:58:34 -0400

    interpolated data for RR

--- Simulation/my_kalman_filter.py
+++ Simulation/my_kalman_filter.py
@@ -31,6 +31,7 @@ import numpy as np
 import csv 
 import matplotlib.pyplot as plt 
 col_to_extract = "value"
+import pandas as pd 
 
 #extract respiratory rate data
 
@@ -52,14 +53,13 @@ with open('/home/asyaakkus/Senior-Project-Modeling-Noah/RespiratoryRate.csv', 'r
 respiratory_rate = np.array(column_data)
 
 
-
-#extract VO_2 data 
-with open('/home/asyaakkus/Senior-Project-Modeling-Noah/VO2Max.csv', 'r') as file: 
+#extract basal energy burned data 
+with open('/home/asyaakkus/Senior-Project-Modeling-Noah/BasalEnergyBurned.csv', 'r') as file: 
     reader = csv.DictReader(file)
 
     column_data = [row[col_to_extract] for row in reader]
 
-vo2_rate = np.array(column_data)
+basal_energy_burned = np.array(column_data)
 
 
 
@@ -68,16 +68,42 @@ def convert_to_integer(array):
     return np.array(array).astype(float)
 
 RR = convert_to_integer(respiratory_rate)
-VO = convert_to_integer(vo2_rate)
+BE = convert_to_integer(basal_energy_burned)
 HR = convert_to_integer(heart_rate)
 
-print(np.shape(RR), np.shape(VO), np.shape(HR))
+print(np.shape(RR), np.shape(BE), np.shape(HR))
 
-print(VO)
-plt.plot(RR)
-plt.show()
+'''
+we find the P matrix by taking the variance of each of the arrays at consistent time stamps. for now, we will just focus on 
+respiratory rate and heart rate, since I am unsure whether or not VO is salvageable data point. There is not enough data 
+and it is too spread out. I am going to use basal energy burned instead. 
+'''
 
-# unified_array = np.array([[RR], [VO], [HR]])
-# P = np.cov(unified_array)
+'''
+We need to truncate the BE and HR arrays to be the same size as RR. With HR, it should be straightforward because both the 
+measurements from the watch are taken in count/min. 
 
-# print(P)
+The timestamps don't align and this is a big problem. We have to probably use the interpolate method in pandas to make this 
+work at all. 
+
+
+'''
+
+#data processing 
+df = pd.read_csv("/home/asyaakkus/Senior-Project-Modeling-Noah/RespiratoryRate.csv")
+
+df['start'] = pd.to_datetime(df['start'])
+df.set_index('start', inplace=True)
+
+common_time = pd.date_range(start=df.index.min(), end=df.index.max(), freq='1T')
+
+respir_interpolated = df['value'].reindex(common_time).interpolate()
+
+aligned_rr_df = pd.DataFrame({
+    'start': common_time, 
+    'value': respir_interpolated
+})
+
+print(aligned_rr_df)
+#unified_array = np.array([[RR], [BE], [HR]])
+#P = np.cov(unified_array)
