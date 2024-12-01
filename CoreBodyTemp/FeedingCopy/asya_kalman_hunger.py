4bca83b8f64efb7842110213df672de9e8c33ef5
Asya Akkus <aya29@case.edu>
2024-11-30 20:16:44 -0500

    progress on hunger levels filter

--- CoreBodyTemp/FeedingCopy/asya_kalman_hunger.py
+++ CoreBodyTemp/FeedingCopy/asya_kalman_hunger.py
@@ -1,59 +1,37 @@
+# imports
+
 import numpy as np 
-import csv 
-import matplotlib.pyplot as plt 
 import os
-import sys
-col_to_extract = "value"
+import csv 
+
 import pandas as pd 
+import matplotlib.pyplot as plt 
+
 from filterpy.kalman import predict
 from filterpy.common import Q_discrete_white_noise
 from filterpy.kalman import KalmanFilter
+import sys 
 sys.path.append("/home/asyaakkus/Senior-Project-Modeling-Noah/SleepCycle")
 from data_processing import process_categorical_data, process_numerical_data, calc_R
 
 
-#extract heart rate data 
-home_dir = "/home/asyaakkus/Senior-Project-Modeling-Noah/data/"
-
-'''
-we find the P matrix by taking the variance of each of the arrays at consistent time stamps. for now, we will just focus on 
-respiratory rate and heart rate, since I am unsure whether or not VO is salvageable data point. There is not enough data 
-and it is too spread out. I am going to use basal energy burned instead. 
-'''
-
-'''
-We need to truncate the BE and HR arrays to be the same size as RR. With HR, it should be straightforward because both the 
-measurements from the watch are taken in count/min. 
-
-The timestamps don't align and this is a big problem. We have to probably use the interpolate method in pandas to make this 
-work at all. 
-
-RR first timestamp: [[Timestamp('2023-07-07 01:08:27-0400', tz='UTC-04:00') 17.0]
-RR last timestamp:  [Timestamp('2024-09-05 08:27:27-0400', tz='UTC-04:00') 11.0]]
-
-
-'''
 #preprocess the data 
-resp_rate_csv_string = "data/RespiratoryRate.csv"
-heart_rate_csv_string = "data/HeartRate.csv"
+physical_csv_string = "data/PhysicalEffort.csv"
 basal_rate_csv_string = "data/BasalEnergyBurned.csv"
 col_interest = 'start'
-processed_respiratory = process_numerical_data(resp_rate_csv_string, col_interest)
-processed_heart_rate = process_numerical_data(heart_rate_csv_string, col_interest)
+processed_phys_rate = process_numerical_data(physical_csv_string, col_interest)
 processed_basal_rate = process_numerical_data(basal_rate_csv_string, col_interest)
 
 #the three arrays have null values, so we crop the nulls out to leave as much valid data as possible  
-processed_respiratory = processed_respiratory[612:]
-processed_heart_rate = processed_heart_rate[612:]
+processed_phys_rate = processed_phys_rate[612:]
 processed_basal_rate = processed_basal_rate[612:]
 
 #thr three arrays are now different lengths, so we find the minimum length and cut off the maximum index of an array at that minimum length 
-min_length = min(len(processed_respiratory), len(processed_heart_rate), len(processed_basal_rate))
+min_length = min(len(processed_phys_rate), len(processed_basal_rate))
 
 #create the index values for the p matrix by finding covariance between the three arrays 
 processed_basal_rate = processed_basal_rate[:min_length]
-processed_heart_rate = processed_heart_rate[:min_length]
-processed_respiratory = processed_respiratory[:min_length]
+processed_phys_rate = processed_phys_rate[:min_length]
 
 
 time_vals = range(0, len(processed_basal_rate))
@@ -62,29 +40,18 @@ time_vals = range(0, len(processed_basal_rate))
 n_steps = len(processed_basal_rate)
 
 # Create zs matrix: (n_steps, 3) where each row corresponds to measurements at one time step
-zs = np.column_stack((processed_basal_rate, processed_heart_rate, processed_respiratory))
+zs = np.column_stack((processed_basal_rate, processed_phys_rate))
 
-
-# Define initial matrices (already provided by you)
-
-# Initial P matrix (state covariance matrix)
-'''We set the covariances for the initial P to be 0 because the matrix has not converged yet. this is 
-just an initialization and so the filter will update the covariances as the model propagates over time. 
-The values were chosen based on the maximum reasonable numerical value for each as determined by a 
-literature search and observation of processed data arrays. 
-'''
 P = np.array([
-        [102, 0, 0, 0], #CBT
-        [0, 22, 0, 0], #BMR
-        [0, 0, 160, 0], #HR
-        [0, 0, 0, 16], #RR
+        [102, 0, 0], #hunger cycle 
+        [0, 22, 0], #BMR
+        [0, 0, 12.5], #PE
     ])
 # Initial state X (based on means of X)
 X = np.array([
-    [97],
+    [1],
     [np.mean(processed_basal_rate[650:])],
-    [np.mean(processed_heart_rate[650:])],
-    [np.mean(processed_respiratory[650:])],
+    [np.mean(processed_phys_rate[650:])],
 ])
 
 # Process model matrix F, equivalent to A in math equations
@@ -92,41 +59,39 @@ dt = 1  # 1 second time step
 
 
 F = np.array([
-    [1, 0, 0, 0],
-    [0, 1, 3.31*np.cos(dt), 0.0001437*np.cos(dt)**2],
-    [0, 0.276*np.cos(dt), 1, -2.713e-6*np.cos(dt)],
-    [0, 1.835e-8, -6.9652e-10, 1],
+    [1, dt, 0.5*dt**2],
+    [0, 1, dt],
+    [0, 0, 1],
 ])
 
 def make_F(theta):
-    return np.array([[1, 0, 0, 0],
-    [0, -np.sin(theta), 3.31*np.cos(theta), 0.0001437*np.cos(theta)**2],
-    [0, 0.276*np.sin(theta), 1, -2.713e-6*np.cos(theta)],
-    [0, 1.835e-8*np.cos(theta), -6.9652e-10*np.cos(theta), 1]])
+    return np.array([[1, 0, 0],
+                     [0, -np.sin(theta), 3.31*np.cos(theta)],
+                     [0, 0.276*np.sin(theta), 1],
+    ])
 
 # Measurement noise covariance matrix R. Basically what we did for P before. Little goof 
-R = calc_R([processed_basal_rate, processed_heart_rate, processed_respiratory])
+R = calc_R([processed_basal_rate, processed_phys_rate])
 print(R)
 
 # Two options for Q (process noise covariance)
-Q_filterpy = Q_discrete_white_noise(dim=4, dt=1., var=7)
+Q_filterpy = Q_discrete_white_noise(dim=3, dt=1., var=7)
+print(Q_filterpy)
 Q_manual = np.array([
-    [0.5, 0, 0, 0],
-    [0, 0.2, 0, 0],
-    [0, 0, 0.3, 0],
-    [0, 0, 0, 0.2],
+    [50, 0, 0],
+    [0, 20, 0],
+    [0, 0, 30],
 ])
 
 # Measurement matrix H (maps state to measurement)
 H = np.array([
-    [0, 1, 0, 0],  # basal rate mapping
-    [0, 0, 1, 0],  # heart rate mapping
-    [0, 0, 0, 1]   # respiratory rate mapping
+    [0, 1, 0],  # basal rate mapping
+    [0, 0, 1],  # phys rate mapping 
 ])
 
 # Kalman filter initialization
 def initialize_kalman_filter(X, P, R, Q, F, H):
-    kf = KalmanFilter(dim_x=4, dim_z=3)
+    kf = KalmanFilter(dim_x=3, dim_z=2)
     kf.x = X
     kf.P = P
     kf.R = R
@@ -176,17 +141,16 @@ def run_kalman_filter(X, P, R, Q, F, H, zs, n_steps):
     return xs_rot, cov, residuals
 
 # Run the Kalman filter with your data
-xs, Ps, residuals = run_kalman_filter(X, P, R, Q_filterpy, F, H, zs, n_steps)
+xs, Ps, residuals = run_kalman_filter(X, P, R, Q_manual, F, H, zs, n_steps)
 
 # xs now contains state estimates, including core body temperature estimates over time
 print(type(xs))
 print(np.shape(xs))
 
 
-xs_reshaped = xs.reshape(612655, 4)
-np.savetxt(os.path.join(home_dir, "predictions_cbt.csv"), xs_reshaped, delimiter=",")
+xs_reshaped = xs.reshape(302171, 3)
 
-xs_cbt = xs_reshaped[:15000, 0]
+xs_cbt = xs_reshaped[:1440, 0]
 ys_cbt = np.arange(len(xs_cbt))
 
 # Calculate Standard Deviation of Residuals and MSE
@@ -216,5 +180,5 @@ plt.legend()
 
 # Show the final plot with both graphs
 plt.tight_layout()
-plt.savefig('my_plot_kal_rot_with_residuals.png')
+plt.savefig('feeding_output.png')
 plt.show()
